40ac7b7204281e72c4ee3bff80538d71a0b039bb
solusipse <solus1ps3@gmail.com>
2014-09-21 02:59:38 +0200

    ignoring osx ds store files

---
+++ stylesheets.cpp
@@ -0,0 +1,5 @@
+#include "stylesheets.h"
+
+Stylesheets::Stylesheets()
+{
+}
---
+++ stylesheets.h
@@ -0,0 +1,10 @@
+#ifndef STYLESHEETS_H
+#define STYLESHEETS_H
+
+class Stylesheets
+{
+public:
+    Stylesheets();
+};
+
+#endif // STYLESHEETS_H
